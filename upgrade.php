0c33867d27147ecbe29db8ef70b52e499c96ad39
Nader732 <swd7496@gmail.com>
2025-05-13 18:24:22 +0300

    Add files via upload

--- upgrade.php
+++ upgrade.php
@@ -1,181 +1,133 @@
 <?php
+
+add_action( 'wpcf7_upgrade', 'wpcf7_upgrade_58', 10, 2 );
+
 /**
- * Upgrade WordPress Page.
+ * Runs functions necessary when upgrading from old plugin versions before 5.8.
  *
- * @package WordPress
- * @subpackage Administration
+ * @since 5.8.0 New `_config_validation` post meta is introduced.
  */
+function wpcf7_upgrade_58( $new_ver, $old_ver ) {
+	if ( ! version_compare( $old_ver, '5.8-dev', '<' ) ) {
+		return;
+	}
+
+	$posts = WPCF7_ContactForm::find( array(
+		'post_status' => 'any',
+		'posts_per_page' => -1,
+	) );
+
+	foreach ( $posts as $post ) {
+		$post_id = $post->id();
+
+		// Delete the old post meta for config-validation results.
+		delete_post_meta( $post_id, '_config_errors' );
+
+		// Add the contact form hash.
+		add_post_meta( $post_id, '_hash',
+			wpcf7_generate_contact_form_hash( $post_id ),
+			true // Unique
+		);
+	}
+}
+
+
+add_action( 'wpcf7_upgrade', 'wpcf7_convert_to_cpt', 10, 2 );
 
 /**
- * We are upgrading WordPress.
+ * Converts old data in the dedicated database table to custom posts.
  *
- * @since 1.5.1
- * @var bool
+ * @since 3.0.0 `wpcf7_contact_form` CPT is introduced.
  */
-define( 'WP_INSTALLING', true );
+function wpcf7_convert_to_cpt( $new_ver, $old_ver ) {
+	global $wpdb;
 
-/** Load WordPress Bootstrap */
-require dirname( __DIR__ ) . '/wp-load.php';
+	if ( ! version_compare( $old_ver, '3.0-dev', '<' ) ) {
+		return;
+	}
 
-nocache_headers();
+	$old_rows = array();
 
-require_once ABSPATH . 'wp-admin/includes/upgrade.php';
+	$table_name = $wpdb->prefix . "contact_form_7";
 
-delete_site_transient( 'update_core' );
+	if ( $wpdb->get_var( "SHOW TABLES LIKE '$table_name'" ) ) {
+		$old_rows = $wpdb->get_results( "SELECT * FROM $table_name" );
+	} elseif ( $opt = get_option( 'wpcf7' )
+	and ! empty( $opt['contact_forms'] ) ) {
+		foreach ( (array) $opt['contact_forms'] as $key => $value ) {
+			$old_rows[] = (object) array_merge(
+				$value,
+				array( 'cf7_unit_id' => $key )
+			);
+		}
+	}
 
-if ( isset( $_GET['step'] ) ) {
-	$step = $_GET['step'];
-} else {
-	$step = 0;
-}
+	foreach ( (array) $old_rows as $row ) {
+		$q = "SELECT post_id FROM $wpdb->postmeta WHERE meta_key = '_old_cf7_unit_id'"
+			. $wpdb->prepare( " AND meta_value = %d", $row->cf7_unit_id );
 
-// Do it. No output.
-if ( 'upgrade_db' === $step ) {
-	wp_upgrade();
-	die( '0' );
-}
-
-/**
- * @global string   $wp_version              The WordPress version string.
- * @global string   $required_php_version    The required PHP version string.
- * @global string[] $required_php_extensions The names of required PHP extensions.
- * @global string   $required_mysql_version  The required MySQL version string.
- * @global wpdb     $wpdb                    WordPress database abstraction object.
- */
-global $wp_version, $required_php_version, $required_php_extensions, $required_mysql_version, $wpdb;
-
-$step = (int) $step;
-
-$php_version   = PHP_VERSION;
-$mysql_version = $wpdb->db_version();
-$php_compat    = version_compare( $php_version, $required_php_version, '>=' );
-if ( file_exists( WP_CONTENT_DIR . '/db.php' ) && empty( $wpdb->is_mysql ) ) {
-	$mysql_compat = true;
-} else {
-	$mysql_compat = version_compare( $mysql_version, $required_mysql_version, '>=' );
-}
-
-$missing_extensions = array();
-
-if ( isset( $required_php_extensions ) && is_array( $required_php_extensions ) ) {
-	foreach ( $required_php_extensions as $extension ) {
-		if ( extension_loaded( $extension ) ) {
+		if ( $wpdb->get_var( $q ) ) {
 			continue;
 		}
 
-		$missing_extensions[] = sprintf(
-			/* translators: 1: URL to WordPress release notes, 2: WordPress version number, 3: The PHP extension name needed. */
-			__( 'You cannot upgrade because <a href="%1$s">WordPress %2$s</a> requires the %3$s PHP extension.' ),
-			$version_url,
-			$wp_version,
-			$extension
+		$postarr = array(
+			'post_type' => 'wpcf7_contact_form',
+			'post_status' => 'publish',
+			'post_title' => maybe_unserialize( $row->title ),
 		);
+
+		$post_id = wp_insert_post( $postarr );
+
+		if ( $post_id ) {
+			update_post_meta( $post_id, '_old_cf7_unit_id', $row->cf7_unit_id );
+
+			$metas = array(
+				'form',
+				'mail',
+				'mail_2',
+				'messages',
+				'additional_settings',
+			);
+
+			foreach ( $metas as $meta ) {
+				update_post_meta( $post_id, '_' . $meta,
+					wpcf7_normalize_newline_deep( maybe_unserialize( $row->{$meta} ) )
+				);
+			}
+		}
 	}
 }
 
-header( 'Content-Type: ' . get_option( 'html_type' ) . '; charset=' . get_option( 'blog_charset' ) );
-?>
-<!DOCTYPE html>
-<html <?php language_attributes(); ?>>
-<head>
-	<meta name="viewport" content="width=device-width, initial-scale=1.0" />
-	<meta http-equiv="Content-Type" content="<?php bloginfo( 'html_type' ); ?>; charset=<?php echo get_option( 'blog_charset' ); ?>" />
-	<meta name="robots" content="noindex,nofollow" />
-	<title><?php _e( 'WordPress &rsaquo; Update' ); ?></title>
-	<?php wp_admin_css( 'install', true ); ?>
-</head>
-<body class="wp-core-ui">
-<p id="logo"><a href="<?php echo esc_url( __( 'https://wordpress.org/' ) ); ?>"><?php _e( 'WordPress' ); ?></a></p>
 
-<?php if ( (int) get_option( 'db_version' ) === $wp_db_version || ! is_blog_installed() ) : ?>
+add_action( 'wpcf7_upgrade', 'wpcf7_prepend_underscore', 10, 2 );
 
-<h1><?php _e( 'No Update Required' ); ?></h1>
-<p><?php _e( 'Your WordPress database is already up to date!' ); ?></p>
-<p class="step"><a class="button button-large" href="<?php echo esc_url( get_option( 'home' ) ); ?>/"><?php _e( 'Continue' ); ?></a></p>
-
-	<?php
-elseif ( ! $php_compat || ! $mysql_compat ) :
-	$version_url = sprintf(
-		/* translators: %s: WordPress version. */
-		esc_url( __( 'https://wordpress.org/documentation/wordpress-version/version-%s/' ) ),
-		sanitize_title( $wp_version )
-	);
-
-	$php_update_message = '</p><p>' . sprintf(
-		/* translators: %s: URL to Update PHP page. */
-		__( '<a href="%s">Learn more about updating PHP</a>.' ),
-		esc_url( wp_get_update_php_url() )
-	);
-
-	$annotation = wp_get_update_php_annotation();
-
-	if ( $annotation ) {
-		$php_update_message .= '</p><p><em>' . $annotation . '</em>';
+/**
+ * Prepends an underscore to post meta keys.
+ */
+function wpcf7_prepend_underscore( $new_ver, $old_ver ) {
+	if ( version_compare( $old_ver, '3.0-dev', '<' ) ) {
+		return;
 	}
 
-	if ( ! $mysql_compat && ! $php_compat ) {
-		$message = sprintf(
-			/* translators: 1: URL to WordPress release notes, 2: WordPress version number, 3: Minimum required PHP version number, 4: Minimum required MySQL version number, 5: Current PHP version number, 6: Current MySQL version number. */
-			__( 'You cannot update because <a href="%1$s">WordPress %2$s</a> requires PHP version %3$s or higher and MySQL version %4$s or higher. You are running PHP version %5$s and MySQL version %6$s.' ),
-			$version_url,
-			$wp_version,
-			$required_php_version,
-			$required_mysql_version,
-			$php_version,
-			$mysql_version
-		) . $php_update_message;
-	} elseif ( ! $php_compat ) {
-		$message = sprintf(
-			/* translators: 1: URL to WordPress release notes, 2: WordPress version number, 3: Minimum required PHP version number, 4: Current PHP version number. */
-			__( 'You cannot update because <a href="%1$s">WordPress %2$s</a> requires PHP version %3$s or higher. You are running version %4$s.' ),
-			$version_url,
-			$wp_version,
-			$required_php_version,
-			$php_version
-		) . $php_update_message;
-	} elseif ( ! $mysql_compat ) {
-		$message = sprintf(
-			/* translators: 1: URL to WordPress release notes, 2: WordPress version number, 3: Minimum required MySQL version number, 4: Current MySQL version number. */
-			__( 'You cannot update because <a href="%1$s">WordPress %2$s</a> requires MySQL version %3$s or higher. You are running version %4$s.' ),
-			$version_url,
-			$wp_version,
-			$required_mysql_version,
-			$mysql_version
-		);
+	if ( ! version_compare( $old_ver, '3.3-dev', '<' ) ) {
+		return;
 	}
 
-	echo '<p>' . $message . '</p>';
-elseif ( count( $missing_extensions ) > 0 ) :
-	echo '<p>' . implode( '</p><p>', $missing_extensions ) . '</p>';
-else :
-	switch ( $step ) :
-		case 0:
-			$goback = wp_get_referer();
-			if ( $goback ) {
-				$goback = sanitize_url( $goback );
-				$goback = urlencode( $goback );
+	$posts = WPCF7_ContactForm::find( array(
+		'post_status' => 'any',
+		'posts_per_page' => -1,
+	) );
+
+	foreach ( $posts as $post ) {
+		$props = $post->get_properties();
+
+		foreach ( $props as $prop => $value ) {
+			if ( metadata_exists( 'post', $post->id(), '_' . $prop ) ) {
+				continue;
 			}
-			?>
-	<h1><?php _e( 'Database Update Required' ); ?></h1>
-<p><?php _e( 'WordPress has been updated! Next and final step is to update your database to the newest version.' ); ?></p>
-<p><?php _e( 'The database update process may take a little while, so please be patient.' ); ?></p>
-<p class="step"><a class="button button-large button-primary" href="upgrade.php?step=1&amp;backto=<?php echo $goback; ?>"><?php _e( 'Update WordPress Database' ); ?></a></p>
-			<?php
-			break;
-		case 1:
-			wp_upgrade();
 
-			$backto = ! empty( $_GET['backto'] ) ? wp_unslash( urldecode( $_GET['backto'] ) ) : __get_option( 'home' ) . '/';
-			$backto = esc_url( $backto );
-			$backto = wp_validate_redirect( $backto, __get_option( 'home' ) . '/' );
-			?>
-	<h1><?php _e( 'Update Complete' ); ?></h1>
-	<p><?php _e( 'Your WordPress database has been successfully updated!' ); ?></p>
-	<p class="step"><a class="button button-large" href="<?php echo $backto; ?>"><?php _e( 'Continue' ); ?></a></p>
-			<?php
-			break;
-endswitch;
-endif;
-?>
-</body>
-</html>
+			update_post_meta( $post->id(), '_' . $prop, $value );
+			delete_post_meta( $post->id(), $prop );
+		}
+	}
+}
